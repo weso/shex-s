7d4468634c46ad281a7f6b29275c4b9497a41064
Jose Labra <jelabra@gmail.com>
2019-11-26 22:55:34 +0100

    Added representation test

--- modules/shexTest/src/main/scala/es/weso/shextest/manifest/ValidateManifest.scala
+++ modules/shexTest/src/main/scala/es/weso/shextest/manifest/ValidateManifest.scala
@@ -10,6 +10,15 @@ import scala.util.{Either, Left, Right, Try}
 import cats.data.EitherT
 import cats.effect.IO
 import cats.implicits._
+import es.weso.shex._
+import es.weso.shex.validator.{ExternalIRIResolver, Validator}
+import es.weso.shex.compact.CompareSchemas
+import es.weso.shextest.manifest.Utils._
+import es.weso.shex.implicits.decoderShEx._
+import es.weso.shex.implicits.encoderShEx._
+import scala.io._
+import io.circe.parser._
+import io.circe.syntax._
 
 trait RunManifest {
 
@@ -55,7 +64,7 @@ trait ValidateManifest extends FunSpec with Matchers with TryValues with OptionV
 
   def parseManifest(name: String, folder: String, parentFolder: String, nameIfSingle: Option[String]): Unit = {
     it(s"Should parse manifestTest $folder/$name") {
-      runManifest(name, folder, parentFolder, nameIfSingle, processEntry)
+      runManifest(name, folder, parentFolder, nameIfSingle, processEntryValidating)
     }
   }
 
@@ -92,6 +101,73 @@ trait ValidateManifest extends FunSpec with Matchers with TryValues with OptionV
     case _ => EitherT.fromEither(Left(s"Unsupported entry type: ${e.entryType}"))
   }
 } else EitherT.pure(None)
+
+def processEntryValidating(
+  e: es.weso.shextest.manifest.Entry,
+  name: String,
+  manifestFolder: String,
+  nameIfSingle: Option[String]
+): EitherT[IO, String, Option[(String, Boolean)]] = 
+if (nameIfSingle == None || nameIfSingle.getOrElse("") == e.name) {
+e match {
+ case r: RepresentationTest => {
+  val manifestFolderURI = Paths.get(manifestFolder).normalize.toUri
+  val resolvedJson = mkLocal(r.json,schemasBase,manifestFolderURI)// IRI(shexFolderURI).resolve(r.json).uri
+  val resolvedShEx = mkLocal(r.shex,schemasBase,manifestFolderURI)// IRI(shexFolderURI).resolve(r.shex).uri
+// info(s"Entry: $r with json: ${resolvedJsonIri}")
+val jsonStr   = Source.fromURI(resolvedJson)("UTF-8").mkString
+val schemaStr = Source.fromURI(resolvedShEx)("UTF-8").mkString
+Schema.fromString(schemaStr, "SHEXC", None) match {
+  case Right(schema) => {
+    decode[Schema](jsonStr) match {
+      case Left(err) => fail(s"Error parsing Json ${r.json}: $err")
+      case Right(expectedSchema) =>
+        if (CompareSchemas.compareSchemas(schema, expectedSchema)) {
+          parse(jsonStr) match {
+            case Left(err) => fail(s"Schemas are equal but error parsing Json $jsonStr")
+            case Right(json) => {
+              if (json.equals(schema.asJson)) {
+                ok(Some((name,true)))
+              } else {
+                fail(
+                  s"Json's are different\nSchema:${schema}\nJson generated: ${schema.asJson.spaces2}\nExpected: ${json.spaces2}")
+              }
+            }
+          }
+        } else {
+          fail(s"Schemas are different. Parsed:\n${schema}\n-----Expected:\n${expectedSchema}")
+        }
+    }
+  }
+  case Left(e) => fail(s"Error parsing Schema: ${r.shex}: $e")
+}
+}
+
+
+case v: Validate => {
+  val a = v.action
+  val r = for {
+    strRdf            <- getContents("data", manifestFolder, a.data)
+    strSchema         <- getContents("schema", manifestFolder, a.schema)
+    strShapeMap       <- getContents("shapeMap", manifestFolder, a.shapeMap)
+    strResultShapeMap <- getContents("resultShapeMap", manifestFolder, a.resultShapeMap)
+  } yield {
+    (strRdf, strSchema, strShapeMap, strResultShapeMap)
+  }
+  r.fold(
+    e => fail(s"Error: $e"),
+    v => {
+      val (strRdf, strSchema, strShapeMap, strResultShapeMap) = v
+      shouldValidateWithShapeMap(name, strRdf, strSchema, strShapeMap, strResultShapeMap).map(Some(_))
+    }
+  )
+}
+case _ => fail(s"Unsupported entry type: ${e.entryType}")
+}
+} else EitherT.pure(None)
+
+ def fail[A](msg: String): EitherT[IO,String,A] = EitherT.fromEither(Left(msg))
+ def ok[A](x:A): EitherT[IO,String,A] = EitherT.pure(x)
 
  def getContents(name: String, folder: String, value: Option[IRI]): Either[String, String] = value match {
     case None      => Left(s"No value for $name")
