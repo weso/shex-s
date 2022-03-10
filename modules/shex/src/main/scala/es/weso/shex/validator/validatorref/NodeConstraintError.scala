a14174033276f6f81fd332560b3529ca2d7f4d91
Jose Labra <jelabra@gmail.com>
2022-03-10 11:24:28 +0100

    Mode code on validatorRef

--- modules/shex/src/main/scala/es/weso/shex/validator/validatorref/NodeConstraintError.scala
+++ modules/shex/src/main/scala/es/weso/shex/validator/validatorref/NodeConstraintError.scala
@@ -1,12 +1,3 @@
 package es.weso.shex.validator.validatorref
 
-package es.weso.shex.validator.validatorref
-
-import es.weso.shex.NodeConstraint
-import cats.effect.IO
-import es.weso.rdf.nodes.RDFNode
-import cats.data.Validated
-
-object NodeConstraintValidator {
-    def validateNodeConstraint(node: RDFNode, nc: NodeConstraint): Validated[NodeConstraintError, RDFNode] = ???
-}
+sealed abstract class NodeConstraintError
--- modules/shex/src/main/scala/es/weso/shex/validator/validatorref/NodeConstraintValidator.scala
+++ modules/shex/src/main/scala/es/weso/shex/validator/validatorref/NodeConstraintValidator.scala
@@ -1,3 +1,10 @@
 package es.weso.shex.validator.validatorref
 
-sealed abstract class NodeConstraintError
+import es.weso.shex.NodeConstraint
+import cats.effect.IO
+import cats.data.Validated
+import es.weso.rdf.nodes.RDFNode
+
+object NodeConstraintValidator {
+    def validateNodeConstraint(node: RDFNode, nc: NodeConstraint): Validated[NodeConstraintError, RDFNode] = ???
+}
