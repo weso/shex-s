65cc6fb8afdc3f627274c5ae6945e45eb488143d
Scala Steward <scala_steward@virtuslab.com>
2022-09-01 21:16:28 +0000

    Revert commit(s) b1491db, 0202b24, 7ed5095

--- modules/wshex/src/main/scala/es/weso/wshex/matcher/Matcher.scala
+++ modules/wshex/src/main/scala/es/weso/wshex/matcher/Matcher.scala
@@ -111,12 +111,12 @@ case class Matcher(
     // val ms =
     //  s.termConstraints.map(tc => matchTermConstraint(tc, entity, s, current))
     // ms.foldLeft(matchExpr) { case (c, current) => c.and(current) }
-    s.termConstraints.foldLeft(matchExpr) { case (current, tc) =>
+    s.termConstraints.foldLeft(matchExpr) { case (current, tc) => {
       current match {
         case nm: NoMatching => nm // TODO: Maybe we could provide more info...
-        case m: Matching    => matchTermConstraint(tc, entity, s, m.entity)
+        case m: Matching => matchTermConstraint(tc, entity, s, m.entity)
       }
-    }
+    }}
   }
 
   private def matchTermConstraint(
